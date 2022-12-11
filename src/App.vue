a5da354183a9f7fcf799c6b4d0d2ca2d869a968e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-12-11 13:23:21 -0500

    Finish desktop styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <main
-    class="relative rounded-xl bg-gray-700 my-auto mx-0 flex flex-col items-center text-center gap-6 px-4 py-7"
+    class="relative rounded-xl bg-gray-700 my-auto mx-0 flex flex-col items-center text-center gap-7 px-4 py-7 max-w-lg"
   >
     <header class="uppercase tracking-[.15em] text-emerald-500 text-xs">
       Advice # 117
     </header>
     <blockquote
-      class="text-white text-xl before:content-['“'] after:content-['”']"
+      class="text-white text-xl before:content-['“'] after:content-['”'] px-4"
     >
       It is easy to sit up and take notice, what's difficult is getting up and
       taking action.
