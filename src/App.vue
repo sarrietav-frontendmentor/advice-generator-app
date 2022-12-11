4dbac285e9df080a1a3ba1cfb7e3c647fa479228
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-12-11 13:55:52 -0500

    Add fallback values while fetching

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     class="relative rounded-xl bg-gray-700 my-auto mx-0 flex flex-col items-center text-center gap-7 px-4 py-7 max-w-lg"
   >
     <header class="uppercase tracking-[.15em] text-emerald-500 text-xs">
-      Advice # 117
+      Advice #{{ quote.id }}
     </header>
     <blockquote
       class="text-white text-xl before:content-['“'] after:content-['”'] px-4"
     >
-      {{ quote }}
+      {{ quote.advice }}
     </blockquote>
     <picture>
       <img src="./assets/pattern-divider-mobile.svg" alt="Divider" />
@@ -35,14 +35,22 @@
 import axios from "axios";
 import { onMounted, ref } from "vue";
 
-const quote = ref("");
+const quote = ref({
+  id: "?",
+  advice: "Loading...",
+});
 
 onMounted(() => {
   fetchQuote();
 });
 
 const fetchQuote = async () => {
+  quote.value = {
+    id: "?",
+    advice: "Loading...",
+  };
   const response = await axios.get("https://api.adviceslip.com/advice");
-  quote.value = response.data.slip.advice;
+  quote.value.id = response.data.slip.id;
+  quote.value.advice = response.data.slip.advice;
 };
 </script>
