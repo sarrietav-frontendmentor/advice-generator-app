0a78eb81d3fdad4b3ef61673633b032f0b1d851c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-12-11 12:51:46 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="bg-slate-500">
+    <header>Advice # 117</header>
+    <blockquote>
+      It is easy to sit up and take notice, what's difficult is getting up and
+      taking action.
+    </blockquote>
+    <picture>
+      <img src="./assets/pattern-divider-mobile.svg" alt="Divider" />
+
+      <source
+        srcset="./assets/pattern-divider-desktop.svg"
+        media="(min-width: 1024px)"
+      />
+    </picture>
+    <button>
+      <img src="./assets/icon-dice.svg" alt="Dice icon" />
+    </button>
+  </main>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+      manroped: ["Manrope", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
