f22b6c2e1911a11e689a55cb71941af9d493ad98
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-12-11 14:09:00 -0500

    Add disabled state to the button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main
-    class="relative rounded-xl bg-gray-700 my-auto mx-0 flex flex-col items-center text-center gap-7 px-4 py-7 max-w-lg"
+    class="relative rounded-xl bg-gray-700 my-auto mx-0 flex flex-col items-center text-center gap-7 px-4 py-7 max-w-lg shadow-md lg:w-[32rem]"
   >
     <header class="uppercase tracking-[.15em] text-emerald-500 text-xs">
       Advice #{{ quote.id }}
@@ -20,12 +20,13 @@
     </picture>
     <button
       @click="fetchQuote"
-      class="rounded-full flex justify-center items-center bg-emerald-400 p-5 -mb-[3.75rem] mt-2 hover:shadow-emerald-300 hover:shadow-[0_0_20px_2px] active:shadow-emerald-300 active:shadow-[0_0_15px_2px] transition-all group"
+      class="rounded-full flex justify-center items-center bg-emerald-400 p-5 -mb-[3.75rem] mt-2 hover:shadow-emerald-300 hover:shadow-[0_0_20px_2px] active:shadow-emerald-300 active:shadow-[0_0_15px_2px] transition-all group disabled:bg-zinc-500 disabled:hover:shadow-none disabled:active:shadow-none disabled:hover:animate-none"
+      :disabled="isDisabled"
     >
       <img
         src="./assets/icon-dice.svg"
         alt="Dice icon"
-        class="group-hover:animate-spin transition-all group-active:animate-spin-shrink"
+        class="group-hover:animate-spin transition-all group-active:animate-spin-shrink group-disabled:opacity-30 group-disabled:animate-none"
       />
     </button>
   </main>
@@ -34,6 +35,8 @@
 <script lang="ts" setup>
 import axios from "axios";
 import { onMounted, ref } from "vue";
+
+const isDisabled = ref(false);
 
 const quote = ref({
   id: "?",
@@ -50,7 +53,14 @@ const fetchQuote = async () => {
     advice: "Loading...",
   };
   const response = await axios.get("https://api.adviceslip.com/advice");
+
+  isDisabled.value = true;
+
   quote.value.id = response.data.slip.id;
   quote.value.advice = response.data.slip.advice;
+
+  setTimeout(() => {
+    isDisabled.value = false;
+  }, 2000);
 };
 </script>
